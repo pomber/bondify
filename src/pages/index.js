95db8619e9b1e9c49b87cc99228c4d2370ea68cc
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-10-19 11:59:21 -0300

    More styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,26 +30,26 @@ function timeDifference(a, b) {
   const seconds = Math.round((b - a) / 1000);
   if (Math.abs(seconds) <= 60) {
     if (seconds < -1) {
-      return ["se fue hace", -seconds, "segundos"];
+      return ["se fue hace", -seconds, "segundos", true];
     } else if (seconds === -1) {
-      return ["se fue hace", -seconds, "segundo"];
+      return ["se fue hace", -seconds, "segundo", true];
     } else if (seconds === 1) {
-      return ["llega en", seconds, "segundo"];
+      return ["llega en", seconds, "segundo", false];
     } else {
-      return ["llega en", seconds, "segundos"];
+      return ["llega en", seconds, "segundos", false];
     }
   }
 
   const minutes = Math.round(seconds / 60);
 
   if (minutes < -1) {
-    return ["se fue hace", -minutes, "minutos"];
+    return ["se fue hace", -minutes, "minutos", true];
   } else if (minutes === -1) {
-    return ["se fue hace", -minutes, "minuto"];
+    return ["se fue hace", -minutes, "minuto", true];
   } else if (minutes === 1) {
-    return ["llega en", minutes, "minuto"];
+    return ["llega en", minutes, "minuto", false];
   } else {
-    return ["llega en", minutes, "minutos"];
+    return ["llega en", minutes, "minutos", false];
   }
 }
 
@@ -59,42 +59,67 @@ function getBusNumberAndCode(bus) {
   return { number, code };
 }
 
+const timeWidth = 64;
+const restWidth = `calc(100% - ${timeWidth}px)`;
+const arrivalHeight = 78;
+const rowHeight = 78 + 4;
+
 function Arrival({ arrival, now }) {
   const { bus, description, arrival: time } = arrival;
   const { number, code } = getBusNumberAndCode(bus);
-  const [time1, time2, time3] = timeDifference(now, new Date(time));
+  const [time1, time2, time3, isGone] = timeDifference(now, new Date(time));
   return (
     <div
       style={{
         width: "100%",
         boxSizing: "border-box",
-        padding: "10px",
-        background: "rgba(20,20,20,0.75)",
-        color: "#fafafa",
+        padding: "0 10px",
         display: "flex",
-        justifyContent: "space-between"
+        justifyContent: "center",
+        alignItems: "center",
+        transition: "height 0.2s ease-in",
+        height: isGone ? 0 : rowHeight,
+        overflow: "hidden"
       }}
     >
-      <div style={{ width: "80%" }}>
-        <div style={{ fontSize: "32px" }}>
-          <strong>{number}</strong>
-          <span style={{ color: "#ccc" }}>{code}</span>
-        </div>
-        <div className="ellipsis" style={{ fontSize: "0.8em" }}>
-          {description}
-        </div>
-      </div>
       <div
         style={{
-          width: "64px",
+          width: "100%",
+          boxSizing: "border-box",
+          padding: "10px",
+          paddingLeft: "20px",
+          background: "rgba(20,20,20,0.75)",
+          color: "#fafafa",
+          borderRadius: "5px",
           display: "flex",
-          flexDirection: "column",
-          textAlign: "center"
+          justifyContent: "space-between",
+          height: arrivalHeight
         }}
       >
-        <div style={{ fontSize: "0.5em", color: "#ccc" }}>{time1}</div>
-        <div style={{ fontSize: "28px" }}>{time2}</div>
-        <div style={{ fontSize: "0.5em", color: "#ccc" }}> {time3}</div>
+        <div style={{ width: restWidth }}>
+          <div style={{ fontSize: "32px" }}>
+            <strong>{number}</strong>
+            <span style={{ color: "#ccc" }}>{code}</span>
+          </div>
+          <div
+            className="ellipsis"
+            style={{ fontSize: "15px", lineHeight: "18px", color: "#eee" }}
+          >
+            {description}
+          </div>
+        </div>
+        <div
+          style={{
+            width: timeWidth,
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center"
+          }}
+        >
+          <div style={{ fontSize: "0.5em", color: "#ccc" }}>{time1}</div>
+          <div style={{ fontSize: "28px" }}>{time2}</div>
+          <div style={{ fontSize: "0.5em", color: "#ccc" }}> {time3}</div>
+        </div>
       </div>
     </div>
   );
@@ -149,7 +174,7 @@ export default function() {
           maxHeight: "100%"
         }}
       >
-        <div style={{ height: "80%" }} />
+        <div style={{ height: `calc(100% - ${rowHeight * 3.5}px` }} />
         <Arrivals arrivals={arrivals} />
       </div>
     </div>
